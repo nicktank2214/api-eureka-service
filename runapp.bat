377aab17485962e27cc1abefbc74f662051e28a5
Nick Tankard <nicktankard@gmail.com>
2019-02-01 16:10:52 +0000

    Commit

---
+++ runapp.bat
@@ -0,0 +1,3 @@
+c:
+cd \armadilloit\armadilloapi\eureka-service
+java -jar c:\armadilloit\armadilloapi\eureka-service\target\eureka-service-0.0.1-SNAPSHOT.jar
---
+++ runbuild.bat
@@ -0,0 +1,3 @@
+c:
+cd \armadilloit\armadilloapi\eureka-service
+C:\apache-maven-3.5.4\bin\mvn package
